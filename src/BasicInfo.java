4afd9066e3c3b54e01f333a4db3c1bda3d6abafe
sacruss <pawelflor1994@gmail.com>
2019-09-15 13:04:13 +0200

    Add BasicInfo - task for use different types of data

---
+++ src/BasicInfo.java
@@ -0,0 +1,15 @@
+public class BasicInfo
+{
+    public static void main(String[] args) {
+        System.out.print("Cześć Jan, wiem, że masz ");
+        System.out.print(25);
+        System.out.println(" lat i uczysz się programowania. Czy to prawda?");
+        System.out.println(true);
+        System.out.print("Czy Twoje inicjały to ");
+        System.out.print('J');
+        System.out.print(" ");
+        System.out.println('K');
+        System.out.println("Nie, " + false);
+
+    }
+}
