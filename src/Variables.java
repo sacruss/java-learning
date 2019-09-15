476a2f62967e03eac24ada20ea3814569a267684
sacruss <pawelflor1994@gmail.com>
2019-09-15 13:58:52 +0200

    New chapter - variables. New code explaining and using variables

---
+++ src/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String[] args) {
+        String firstName = "Jan";
+        int age  = 25;
+        double height = 180.5;
+        String lastName = "Kowalski";
+        final String pesel = "1234567890";
+
+        System.out.println(firstName + " " + lastName + " ma " + age + " lat");
+        System.out.println("Pan " +  lastName + " ma pesel " + pesel);
+        System.out.println("Jan ma wzrost 180cm");
+        System.out.println("Jeżeli firstName urośnie o 5cm, to będzie miał " + (height + 5) + "cm");
+
+        firstName = "Krzysztof";
+        System.out.println("Imię po zmianie " + firstName);
+        
+    }
+}
