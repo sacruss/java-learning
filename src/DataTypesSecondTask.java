8e8c915559ad3a46faf3deb60205c1e1968b6a3f
sacruss <pawelflor1994@gmail.com>
2019-09-15 13:10:55 +0200

    Data types - add second task

---
+++ src/DataTypesSecondTask.java
@@ -0,0 +1,9 @@
+public class DataTypesSecondTask {
+    public static void main(String[] args) {
+        System.out.println("Mam audi A4");
+        System.out.print("Rocznik: ");
+        System.out.println(2004);
+        System.out.print("Silnik o pojemności: ");
+        System.out.println(2.0);
+    }
+}
