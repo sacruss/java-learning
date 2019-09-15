1551c6c9b3b6d06d00621fa955a2c6be3506896d
sacruss <pawelflor1994@gmail.com>
2019-09-15 14:26:33 +0200

    Add information about the accident rate of the car to the first task in the variable section

--- src/VariablesFirstTask.java
+++ src/VariablesFirstTask.java
@@ -4,13 +4,14 @@ public class VariablesFirstTask {
         String modelName;
         int productionYear;
         double cost;
+        boolean accidents = false;
 
         brandName = "Mercedes";
         modelName = "AMG GT";
         productionYear = 2018;
         cost = 799500;
 
-        System.out.println(brandName + " " modelName + " was produced in " + productionYear + " year and costs " + cost);
+        System.out.println(brandName + " " + modelName + " was produced in " + productionYear + " year and costs " + cost + " This car is an accident: " + accidents);
 
     }
 }
