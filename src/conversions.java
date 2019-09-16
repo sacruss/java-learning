992560534cb760227fa487a01717e1b64281341e
sacruss <pawelflor1994@gmail.com>
2019-09-16 12:42:58 +0200

    Learning logical and mathematical operators. A task iwht random numbers (mathAndLogicalOperatorTask). Adding conversions2 with char type and working on it. Change in coversion - narrowing and widening

--- src/conversions.java
+++ src/conversions.java
@@ -7,5 +7,18 @@ public class conversions {
         System.out.println(c);
         System.out.println(d);
 
+        double number1 = 10.987;
+        int number2 = 5;
+
+        //konwersja zawężająca (eng. narrowing)
+
+        int narrowing = (int) number1;
+
+        //konwersja rozszerzająca (eng. widening)
+
+        double widening = (double) number2;
+
+        System.out.println("Narrowing: " + narrowing);
+        System.out.println("Widening: " + widening);
     }
 }
---
+++ src/mathAndLogicalOperatorsTask.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+public class mathAndLogicalOperatorsTask {
+    public static void main(String[] args) {
+
+        Random rand = new Random ();
+        int x = rand.nextInt (10); // losowa liczba z przedziału [0,9], 10 się nie wlicza
+        int y = new Random ().nextInt(10); // krótsza wersja
+        System.out.println("x=" + x + ", y=" + y);
+
+        // is x greater than y?
+        boolean result = x > y;
+        System.out.println("x > y = " + result);
+
+        // is x multiplied by 2 greater than y
+        boolean result2 = x * 2 > y;
+        System.out.println("x * 2 > y = " + result2);
+
+        // is y smaller than the sum of x + 3 and simultaneously greater than x minus 2?
+        boolean result3 = (y < x + 3) && (y > x - 2);
+        System.out.println("(y < x + 3) && (y > x - 2) = " + result3);
+
+        // is the product of x and y even?
+        boolean result4 = ((x * y) % 2 ==0);
+        System.out.println("Is x * y even? " + result4);
+
+
+
+
+    }
+}
